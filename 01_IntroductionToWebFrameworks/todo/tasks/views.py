4b3f3c5c1d63371b6bc7edbe29e2f327af5c84ff
vdstr <vd@stratov.ru>
2021-06-29 19:12:00 +0300

    module 1 rework

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -6,6 +6,7 @@ from django.views import View
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
+        # TODO: Под произвольным выводом дел подразумевается вывод дел в случайном порядке. Предлагаю доработать:)
         return HttpResponse('<ul>'
                             '<li>Установить python</li>'
                             '<li>Установить django</li>'
