fdb25ed4edbad74b30b399af512a5cf791938191
Vitaly Kiryuhin <kiryuha_11@mail.ru>
2021-07-04 18:47:14 +0300

    Summary
    требуется помощь преподавателя

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -5,28 +5,27 @@ from django.views import View
 
 class ToDoView(View):
 
+
+
     def get(self, request, *args, **kwargs):
-        # TODO: Импорты следует делать в начале файла, в конце файла должна быть одна пустая строка:)
         import random
-        # TODO: Хорошо!
-        #  Если делать более профессионально, то лучше сначала задать шаблоны вида
-        #  LI_TEMPLATE = '<li>{}</li>'
-        #  и
-        #  UL_TEMPLATE = '<ul>{}</ul>'
-        #  и наполнять их при помощи метода format()
-        #  Предлагаю попробовать. Если не получится, то я подскажу:)
-        #  Также нейминг должен говорить о том за что отвечает переменная:)
-        h = ["<li>Установить django</li>",
-             "<li>Запустить сервер</li>",
-             "<li>Порадоваться результату</li>",
-             "<li>Рано радоваться</li>",
-             "<li>Попить чай</li>"
-             ]
-        random.shuffle(h)
+        
+        spisokdel = ["Установить django",
+                     "Запустить сервер",
+                     "Порадоваться результату",
+                     "Рано радоваться",
+                     "Попить чай"
+                     ]
+        random.shuffle(spisokdel)
         a = []
-        for i in h:
+        for i in spisokdel:
             a.append(i)
-        return HttpResponse(a)
 
-
-
+        return HttpResponse(
+            "<ul>"
+                "<li>{}</li>".format(a[0]),
+                "<li>{}</li>".format(a[1]),
+                "<li>{}</li>".format(a[2]),
+                "<li>{}</li>".format(a[3]),
+                "<li>{}</li>".format(a[4]),
+            "</ul>")
