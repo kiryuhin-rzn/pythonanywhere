ffebc981a7487f2bc4f215cd27d8673e3c48698c
Vitaly Kiryuhin <kiryuha_11@mail.ru>
2021-07-25 09:48:30 +0300

    Revert "Revert "Исправленная домашка №3""
    This reverts commit 0b544f32d6af14c1c234dba2a991180af77a4db4.

--- 03_RequestsHandlingInDjango/board/board/middleware/log_info_middleware.py
+++ 03_RequestsHandlingInDjango/board/board/middleware/log_info_middleware.py
@@ -1,39 +1,37 @@
-from django.core.files import File
+import os
+from board.settings import BASE_DIR
 import time
 
 
-# TODO: Класс должен быть обособлен от остального кода на 2 пустые строки:)
 class UserInfoTime:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-
         t = time.ctime()
-        # TODO: Код должен работать на любом компьютере и/или сервере, а не только вашем
-        my_file = open('c:/djangoproekt/python_django/03_RequestsHandlingInDjango/board/board/middleware/file.txt', 'a')
+        my_file = open(os.path.join(os.path.dirname(__file__), 'file.txt'), 'a')
         my_file.write(t)
-        # TODO: Забыли "()"
-        my_file.close
+        my_file.close()
 
         response = self.get_response(request)
 
         return response
+
 
 class UserInfoIp:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-
         ip = request.META.get('REMOTE_ADDR')
-        my_file = open('c:/djangoproekt/python_django/03_RequestsHandlingInDjango/board/board/middleware/file.txt', 'a')
+        my_file = open(os.path.join(os.path.dirname(__file__), 'file.txt'), 'a')
         my_file.write(ip)
-        my_file.close
+        my_file.close()
 
         response = self.get_response(request)
 
         return response
+
 
 class UserInfoMethod:
     def __init__(self, get_response):
@@ -41,17 +39,15 @@ class UserInfoMethod:
 
     def __call__(self, request):
         if request.method == 'GET':
-            my_file = open('c:/djangoproekt/python_django/03_RequestsHandlingInDjango/board/board/middleware/file.txt', 'a')
+            my_file = open(os.path.join(os.path.dirname(__file__), 'file.txt'), 'a')
             my_file.write('GET')
-            my_file.close
+            my_file.close()
         if request.method == 'POST':
-            my_file = open('c:/djangoproekt/python_django/03_RequestsHandlingInDjango/board/board/middleware/file.txt', 'a')
+            my_file = open(os.path.join(os.path.dirname(__file__), 'file.txt'), 'a')
             my_file.write('POST')
-            my_file.close
+            my_file.close()
 
         response = self.get_response(request)
 
         return response
-
-
 
